f2eddd12150603fcc3d50b53afe288769cdf98f6
Michael Gerner Andreasen <geekcamp@gmail.com>
2025-07-26 10:32:35 +0200

    Commented out gettimeofday definition for linux

--- src/comm.c
+++ src/comm.c
@@ -169,10 +169,10 @@ int	bind		args( ( int s, struct sockaddr *name, int namelen ) );
 int	getpeername	args( ( int s, struct sockaddr *name, int *namelen ) );
 int	getsockname	args( ( int s, struct sockaddr *name, int *namelen ) );
 int	listen		args( ( int s, int backlog ) );
+int	gettimeofday	args( ( struct timeval *tp, struct timezone *tzp ) );
 */
 
 int	close		args( ( int fd ) );
-int	gettimeofday	args( ( struct timeval *tp, struct timezone *tzp ) );
 int	read		args( ( int fd, char *buf, int nbyte ) );
 int	select		args( ( int width, fd_set *readfds, fd_set *writefds,
 			    fd_set *exceptfds, struct timeval *timeout ) );
